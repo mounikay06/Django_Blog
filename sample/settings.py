1a592e766a3ea76a31917d9a1b57ada0d6bb3ffb
Mounika Yarasi <yarasimounika46@gmail.com>
2020-11-02 12:55:03 +0530

    Added ALLOWED_HOST

--- sample/settings.py
+++ sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&3v54#qflf^-w4#g9%y-_ai0_69pcv2*l3ai@qb*16dhs(+rw4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangoblogpostapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,5 +137,24 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "mounikay06@gmail.com" #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = "yarasi@37"  #os.environ.get('EMAIL_PASS') 
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE =  False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+
+
+
+
+
+
