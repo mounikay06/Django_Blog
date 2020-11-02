87543b4ad9a9ce3d9c1fd57b35530ea8d7d14c13
Mounika Yarasi <yarasimounika46@gmail.com>
2020-11-02 13:42:56 +0530

    Added django-heroku

--- sample/settings.py
+++ sample/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,7 +153,7 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+django_heroku.settings(locals())
 
 
 
