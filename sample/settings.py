c98d10f1cadf56a9fc104cb59eb6fea8228e52b6
Mounika Yarasi <yarasimounika46@gmail.com>
2020-11-03 11:24:30 +0530

    Updated Settings

--- sample/settings.py
+++ sample/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+#STATIC_ROOT = BASE_DIR/'staticfiles'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR/'media'
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR/'media'
+#MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -147,10 +147,11 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY =os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
 AWS_S3_FILE_OVERWRITE =  False
 AWS_DEFAULT_ACL = None
 
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
